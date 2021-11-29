8005a85041683a618085d097c6d7a407b284ce35
nasim <zamaninasim213@gamail.com>
2021-11-29 10:48:44 +0330

    add person class properties & method

--- src/main/java/ir/maktab/Person.java
+++ src/main/java/ir/maktab/Person.java
@@ -1,4 +1,30 @@
 package ir.maktab;
 
 public class Person {
+    private String name;
+    private int lateDays;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLateDays() {
+        return lateDays;
+    }
+
+    public void setLateDays(int lateDays) {
+        this.lateDays = lateDays;
+    }
+
+    public void borrow(Disc disc, Date date) {
+
+    }
+
+    public void deliver(Disc disc, Date date) {
+
+    }
 }
